68676681fa4e81c4771663d64d2a0080c339e7a1
Techtron-tech <techtron.tech@yahoo.com>
2026-04-02 15:20:00 +0100

    added google analytics

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,6 +4,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4F1X8FYK2W"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4F1X8FYK2W');
+            `,
+          }}
+        />
+
         {/* Canonical domain hint */}
         <link rel="canonical" href="https://learnexity.org" />
 
