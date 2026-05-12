5a0725d9b2d257be596cfbf2eba366b4f6f23aea
Techtron-tech <techtron.tech@yahoo.com>
2026-05-12 20:59:46 +0100

    modified config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+
   images: {
     unoptimized: true,
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.learnexity.org",
+      },
+    ],
   },
+
   basePath: "",
   assetPrefix: "/",
 };
